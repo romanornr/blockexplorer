25fc629f5705a6683d98ca11e8c9685086b39889
romanornr <romanornr@gmail.com>
2017-08-01 18:24:52 +0200

    Golang Julian router

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
+	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/spf13/viper"
 )
 
-func main() {
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("website/*"))
+
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("config")
 	viper.SetConfigName("app")
@@ -19,5 +26,20 @@ func main() {
 	}
 
 	fmt.Printf("Reading configuration from %s\n", viper.ConfigFileUsed())
-	fmt.Printf("using %s:%d\n", viper.Get("server.ip"), viper.Get("server.port"))
+}
+
+func main() {
+
+	mux := httprouter.New()
+	mux.GET("/", index)
+	http.ListenAndServe(":8080", mux)
+
+	//fmt.Printf("using %s:%d\n", viper.Get("server.ip"), viper.Get("server.port"))
+}
+
+func index(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	err := tpl.ExecuteTemplate(w, "index.html", nil)
+	if err != nil {
+		log.Println("errror")
+	}
 }

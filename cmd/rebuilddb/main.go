dcd0cfc6bfc74fe5c4788dd8213b8ed790f72638
romanornr <romanornr@gmail.com>
2018-06-10 18:38:32 +0200

    Switch to Boltdb

--- cmd/rebuilddb/main.go
+++ cmd/rebuilddb/main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"github.com/go-redis/redis"
 	"fmt"
-	"log"
-	"github.com/spf13/viper"
+	"github.com/coreos/bbolt"
 	"github.com/romanornr/cyberchain/blockdata"
+	"github.com/spf13/viper"
+	"log"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"runtime"
+	"path"
+	"time"
+	"encoding/binary"
 )
 
-var redisClient *redis.Client
-
-func init(){
-	redisClient = redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-		Password: "",
-		DB: 0,
-	})
+// initalize and read viper configuration
+// Create or Open database with the Open() function
+// Setup the databse with the SetupDB function
+func init() {
 
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("config")
@@ -28,15 +29,79 @@ func init(){
 	}
 
 	fmt.Printf("Reading configuration from %s\n", viper.ConfigFileUsed())
+	Open()
+	setupDB()
 }
 
-func main(){
-	fmt.Println(blockdata.GetBlockHash(0))
+var db *bolt.DB
+var open bool
 
-	//e := blockdata.GetBlockHash(0).String()
-	err := redisClient.Set("0", blockdata.GetBlockHash(0).String(), 0).Err()
-	if err != nil{
-		panic(err)
+// Open or Create a databse in cmd/rebuilddb directory
+func Open() error {
+	var err error
+	_, filename, _, _ := runtime.Caller(0)  // get full path of this file
+	coinsymbol := viper.GetString("coin.symbol") // example: btc or via
+	dbfile := path.Join(path.Dir(filename), coinsymbol+".db")
+	config := &bolt.Options{Timeout: 1 * time.Second}
+	db, err = bolt.Open(dbfile, 0600, config)
+	if err != nil {
+		log.Fatal(err)
 	}
-	//fmt.Println(blockdata.GetDifficulty())
+	open = true
+	return err
+}
+
+// setup database with a bucket called Blocks
+func setupDB() (*bolt.DB, error) {
+	var err error
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err = tx.CreateBucketIfNotExists([]byte("Blocks"))
+		if err != nil {
+			return fmt.Errorf("could not create blocks bucket: %v", err)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("could not setup buckets, %v", err)
+	}
+	fmt.Println("DB Setup Done")
+	return db, nil
+}
+
+var Blocks *chainhash.Hash
+
+// add a block
+func addBlock(db *bolt.DB, blockheight uint64, block *chainhash.Hash) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("Blocks"))
+
+		buf := make([]byte, 8)
+		binary.LittleEndian.PutUint64(buf, blockheight) // to byte little endian
+
+		err := b.Put(buf, []byte(block.CloneBytes()))
+		return err
+	})
+}
+
+func viewBlock(blockheight int64) error{
+	return db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("Blocks"))
+		if bucket == nil{
+			return fmt.Errorf("Bucket not found")
+		}
+		block := bucket.Get([]byte("0"))
+		fmt.Println(block)
+		return nil
+	})
+}
+
+func main() {
+	blockHeight := int64(0)
+	block := blockdata.GetBlockHash(blockHeight)
+
+	fmt.Println("adding block")
+	addBlock(db, uint64(blockHeight), block)
+	viewBlock(blockHeight)
+
 }

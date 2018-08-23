a808b7a98d853117cf004480e422737ca7537a83
oni <onidoru@yahoo.com>
2018-08-23 23:14:28 +0300

    go: refactor server

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"github.com/romanornr/cyberchain/client"
 	"github.com/romanornr/cyberchain/server"
 )
 
 func main() {
 	server.Start()
-	cl := client.GetInstance()
-	cl.Connect(2)
 }

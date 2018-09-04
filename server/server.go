d928671e7ce92484333ad6c80896a4293f269b47
Romano <romanornr@gmail.com>
2018-09-04 13:12:03 +0200

    More server package cleanup

--- server/server.go
+++ server/server.go
@@ -13,15 +13,6 @@ var tpl *template.Template
 
 func init() {
 	tpl = template.Must(template.ParseGlob("website/*"))
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath("config")
-	viper.SetConfigName("app")
-
-	err := viper.ReadInConfig()
-
-	if err != nil {
-		log.Fatal("No configuration file loaded! Please check the config folder")
-	}
 
 	fmt.Printf("Reading configuration from %s\n", viper.ConfigFileUsed())
 	fmt.Printf("Listening on %s:%d\n", viper.GetString("server.ip"), viper.Get("server.port"))
@@ -32,6 +23,7 @@ func Start() {
 	addr := flag.String("addr", port, "http service address")
 
 	flag.Parse()
+	fmt.Println("Server started...")
 
 	router := createRouter()
 	err := http.ListenAndServe(*addr, router)
@@ -39,5 +31,4 @@ func Start() {
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
-
 }

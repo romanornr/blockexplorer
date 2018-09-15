c3b3b4e9d759150b790bcded96857549796eb25d
Romano <romanornr@gmail.com>
2018-09-15 16:57:55 +0200

    [cmd/rebuilddb] index blockheight with hash into boltdb

--- cmd/rebuilddb/rebuild.go
+++ cmd/rebuilddb/rebuild.go
@@ -2,55 +2,37 @@ package rebuilddb
 
 import (
 	_ "github.com/btcsuite/btcd/btcjson"
+	"github.com/romanornr/cyberchain/blockdata"
 	"github.com/romanornr/cyberchain/database"
 	"gopkg.in/cheggaaa/pb.v2"
-	"github.com/romanornr/cyberchain/blockdata"
 	"log"
 )
 
 var db = database.GetDatabaseInstance()
 
 func BuildDatabase() {
-	////database.Open()
-	////database.SetupDB()
-	//
-	//	blockhashChannel := make(chan []byte)
-	////	blockChannel := make(chan *btcjson.GetBlockVerboseResult)
-	//
-	//	go func() {
-	//		for i := int64(1); i < 200; i++ {
-	//			blockhashChannel <- blockdata.GetBlockHashAsync(i).CloneBytes()
-	//		}
-	//		close(blockhashChannel)
-	//	}()
-	//
-	//
-	//
-	//	for a := range blockhashChannel {
-	//		blockhash, _ := chainhash.NewHash(a)
-	//		block, _ := blockdata.GetBlock(blockhash)
-	//		fmt.Println(block)
-	//		database.AddBlock(db, block.Hash,block )
-	//	}
-
 
 	progressBar := pb.StartNew(1000)
 	for i := int64(1); i < 1000; i++ {
-		//blockhash := blockdata.GetBlockHash(i) ==>
-		//fmt.Println(blockdata.GetBlockHash(i))
+
 		blockhash := blockdata.GetBlockHash(i)
 		block, err := blockdata.GetBlock(blockhash)
 		if err != nil {
 			log.Fatal("error")
 		}
-		database.AddBlock(db, blockhash.String(), block)
-		progressBar.Increment()
-		//AddIndexBlockHeightWithBlockHash(db, blockHashString, block.Height)
-		//AddBlock(db, block.Hash, block) ==>
-		//AddTransaction(db, block.Tx)
-		//AddIndexTransactionWithBlockHash(db, blockHashString, block.Tx)
 
+		database.AddBlock(db, blockhash.String(), block)                        // bucket:"Blocks"  key:blockhash  value:blockVerboseResult
+		database.AddIndexBlockHeightWithBlockHash(db, block.Hash, block.Height) //bucket:"Blockheight" key:blockheight value:blockhash
+
+		progressBar.Increment()
 	}
+
 	progressBar.Finish()
+
+	// example txhex to tx
+	//a, _ := hex.DecodeString("010000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff3503d3fa520fe4b883e5bda9e7a59ee4bb99e9b1bc205b323031382d30392d31355431343a32343a32392e3237343736363538315a5dffffffff0294357700000000001976a914b722782e401ed7a31135580ada74962ef32e5cd288ac0000000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf90120000000000000000000000000000000000000000000000000000000000000000000000000")
+	//b, _ := btcutil.NewTxFromBytes(a)
+	//k := b.MsgTx().TxIn[0].SerializeSize()
+	//fmt.Println(string(k))
 
 }

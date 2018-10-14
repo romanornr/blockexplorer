fb1c4276eee98247398ec888f781249a7feac1e2
Romano <romanornr@gmail.com>
2018-10-14 20:05:21 +0200

    [insight] start fix structs

---
+++ insight/address.go
@@ -0,0 +1,23 @@
+package insight
+
+type Address struct {
+	Address      string `json:"address, omitempty"`
+	From         int    `json:"from"`
+	To           int    `json:"to"`
+	Transactions []Tx   `json:"transactions, omitempty"`
+}
+
+type AddressInfo struct {
+	Address                  string   `json:"addrStr,omitempty"`
+	Balance                  float64  `json:"balance"`
+	BalanceSat               int64    `json:"balanceSat"`
+	TotalReceived            float64  `json:"totalReceived"`
+	TotalReceivedSat         int64    `json:"totalReceivedSat"`
+	TotalSent                float64  `json:"totalSent"`
+	TotalSentSat             float64  `json:"totalSentSat"`
+	UnconfirmedBalance       float64  `json:"unconfirmedBalance"`
+	UnconfirmedBalanceSat    float64  `json:"unconfirmedBalanceSat"`
+	UnconfirmedTxAppearances int64    `json:"unconfirmedTxApperances"`
+	TxAppearances            int64    `json:"txApperances"`
+	TransactionsID           []string `json:"transactions,omitempty"`
+}
--- insight/transaction.go
+++ insight/transaction.go
@@ -1,44 +1,19 @@
 package insight
 
-import "github.com/btcsuite/btcd/btcjson"
-
-// insight api compatible address index struct
-type AddrIndex struct {
-	AddrStr                 string   `json:"addrStr"`
-	Balance                 float64  `json:"balance"`
-	BalanceSat              float64  `json:"balanceSat"`
-	TotalReceived           float64  `json:"totalReceived"`
-	TotalReceivedSat        float64  `json:"totalReceivedSat"`
-	TotalSent               float64  `json:"totalSent"`
-	TotalSentSat            float64  `json:"totalSentSat"`
-	UnconfirmedBalance      float64  `json:"unconfirmedBalance"`
-	UnconfirmedBalanceSat   float64  `json:"unconfirmedBalanceSat"`
-	UnconfirmedTxApperances float64  `json:"unconfirmedTxApperances"`
-	TxApperances            uint64   `json:"txApperances"`
-	Transactions            []string `json:"transactions"`
+type Tx struct {
+	Txid          string  `json:"txid,omitempty"`
+	Version       int32   `json:"version,omitempty"`
+	Locktime      uint32  `json:"locktime"`
+	IsCoinBase    bool    `json:"isCoinBase,omitempty"`
+	Vins          []*Vin  `json:"vin,omitempty"`
+	Vouts         []*Vout `json:"vout,omitempty"`
+	Blockhash     string  `json:"blockhash,omitempty"`
+	Blockheight   int64   `json:"blockheight"`
+	Confirmations int64   `json:"confirmations"`
+	Time          int64   `json:"time,omitempty"`
+	Blocktime     int64   `json:"blocktime,omitempty"`
+	ValueOut      float64 `json:"valueOut,omitempty"`
+	Size          uint32  `json:"size,omitempty"`
+	ValueIn       float64 `json:"valueIn,omitempty"`
+	Fees          float64 `json:"fees,omitempty"`
 }
-
-// compatible insight tx struct
-// MISSING: inside tge btcjson.TxRawResult there's []vin and []vout
-// which also need exta fields
-type TxRawResult struct {
-	*btcjson.TxRawResult
-	ValueOut float64 `json:"valueOut"`
-	ValueIn  float64 `json:"valueIn"`
-	Fees     float64 `json:"fees"`
-}
-
-// How []vin should look like. Address, ValueSatoshi and DoubleSpentTxID need
-// to be added to the vin struct
-//type Vin struct {
-//	Coinbase        string      `json:"coinbase"`
-//	Txid            string      `json:"txid"`
-//	Vout            uint32      `json:"vout"`
-//	ScriptSig       *ScriptSig  `json:"scriptSig"`
-//	Sequence        uint32      `json:"sequence"`
-//	Witness         []string    `json:"txinwitness"`
-//	Address         string      `json:"addr"`
-//	ValueSatoshi    int64       `json:"valueSat"`
-//	DoubleSpentTxID interface{} `json:"doubleSpentTxID"`
-//}
-

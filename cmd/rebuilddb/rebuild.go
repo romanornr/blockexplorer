c8bf22c3c2efb8d80d283fd6115f5cebf0e9e1e9
Romano <romanornr@gmail.com>
2018-10-29 17:23:58 +0100

    [rebuilddb] add blocks to mongodb

--- cmd/rebuilddb/rebuild.go
+++ cmd/rebuilddb/rebuild.go
@@ -1,178 +1,171 @@
 package rebuilddb
 
 import (
-	"bytes"
-	"encoding/gob"
-	"fmt"
-	"log"
-	"sync"
-
-	"github.com/btcsuite/btcd/btcjson"
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	_ "github.com/romanornr/cyberchain/address"
+	"github.com/romanornr/cyberchain/mongodb"
 	"github.com/romanornr/cyberchain/blockdata"
-	"github.com/romanornr/cyberchain/database"
-	"github.com/romanornr/cyberchain/insight"
-	"gopkg.in/cheggaaa/pb.v2"
 )
 
-var db = database.GetDatabaseInstance()
+//var db = database.GetDatabaseInstance()
 
-/*
+var db = mongodb.GetSession()
+
+/*  THIS WAS IN BBOLT/BOLTDB
 	note: 2000 blocks costs currently 8.4 MB and ~39 seconds to save. Running into performance issues.
 	goroutine "go addTransactions(block) speed up from ~39 seconds to ~29 seconds. 25% speed up
 
 	try to analyze this address: https://chainz.cryptoid.info/via/address.dws?369935.htm
 */
 
+/*
+	MongoDB
+	2000 blocks without transactions cost 1.746 seconds
+ */
 func BuildDatabase() {
-
-	startblock := 5422072 - 1
-	currentBlock := 5422072 + 10
-	progressBar := pb.StartNew(currentBlock)
-	wg := sync.WaitGroup{}
-	wg.Add(currentBlock - startblock)
-
-	for i := int64(startblock); i < int64(currentBlock); i++ {
-		go resolveBlockToDB(i, progressBar, &wg)
+	for i := 1; i < 2000; i ++ {
+		blockhash := blockdata.GetBlockHash(int64(i))
+		block, _ := blockdata.GetBlock(blockhash)
+		mongodb.AddBlock(block)
 	}
-
-	wg.Wait()
-	progressBar.Finish()
-	test()
 }
 
-func resolveBlockToDB(i int64, prBar *pb.ProgressBar, callerWG *sync.WaitGroup) {
-	blockhash := blockdata.GetBlockHash(i)
-	block, err := blockdata.GetBlock(blockhash)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	localWG := sync.WaitGroup{}
-	localWG.Add(3)
-
-	// adding transactions to the "Transactions" bucket
-	go func() {
-		defer localWG.Done()
-		for j := 0; j < len(block.Tx); j++ {
-			txhash, _ := chainhash.NewHashFromStr(block.Tx[j])
-			tx := blockdata.GetRawTransactionVerbose(txhash)
-
-			//Needs to be changed
-			insighttx := insight.TxRawResult{
-				tx,
-				0.022,
-				0.1,
-				200.10,
-			}
-
-			fmt.Println(insighttx)
-
-			database.AddTransaction(db, insighttx)
-			resolveAddresses(tx)
-		}
-	}()
-
-	go func() {
-		defer localWG.Done()
-
-		// bucket:"Blocks"  key:blockhash  value:blockVerboseResult
-		database.AddBlock(db, blockhash.String(), block)
-	}()
-
-	go func() {
-		defer localWG.Done()
-
-		// bucket:"Blockheight" key:blockheight value:blockhash
-		database.AddIndexBlockHeightWithBlockHash(db, block.Hash, block.Height)
-	}()
-
-	localWG.Wait()
-	callerWG.Done()
-	prBar.Increment()
-}
-
-// index adresses by calculating every transaction, balance etc
-// example: https://explorer.viacoin.org/api/addr/Vrh9ro5WhykxrPPBe2cgyNiB2sAVqzkWjX
-func resolveAddresses(transaction *btcjson.TxRawResult) {
-
-	var addr insight.AddrIndex
-
-	addr.AddrStr = transaction.Vout[0].ScriptPubKey.Addresses[0]
-	addr.TotalReceived = transaction.Vout[0].Value
-	addr.TotalReceivedSat = addr.TotalReceived * 100000000
-	//address.TotalSent
-	//address.TotalSentSat
-	addr.UnconfirmedBalance = 0
-	addr.UnconfirmedTxApperances = 0
-	addr.Transactions = append(addr.Transactions, transaction.Txid)
-
-	//fmt.Println(addr.AddrStr)
-	//fmt.Println(transaction.Txid)
-	//fmt.Println(addr.TotalReceived)
-	fmt.Println(transaction.Vin[0].ScriptSig)
-
-	// Check if address was already in the database
-	// use old values to calculate the new values.
-	// addrInDatabase has all the values of what is already in the database
-	var addrInDatabase insight.AddrIndex
-	addressInDatabase := database.ViewAddress(db, addr.AddrStr)
-	if len(addressInDatabase) > 1 {
-		decoder := gob.NewDecoder(bytes.NewReader(addressInDatabase))
-		decoder.Decode(&addrInDatabase)
-
-		addr.TotalReceived += addrInDatabase.TotalReceived
-		addr.TotalReceivedSat += addrInDatabase.TotalReceivedSat
-		addr.Transactions = append(addr.Transactions, addrInDatabase.Transactions[0])
-
-		// delete old key in the database so the updated one can be inserted instead
-		database.DeleteAddress(db, addr.AddrStr)
-
-	}
-
-	database.IndexAdress(db, addr)
-
-	//var addr address.Index
-	//
-	//for i := 0; i < len(transaction.Vout); i++ {
-	//	for j := 0; i < len(transaction.Vout[i].ScriptPubKey.Addresses); j++ {
-	//		addr.AddrStr = transaction.Vout[i].ScriptPubKey.Addresses[j]
-	//		addr.TotalReceived = transaction.Vout[i].Value
-	//		addr.TotalReceivedSat = addr.TotalReceived * 100000000
-	//		//address.TotalSent
-	//		//address.TotalSentSat
-	//		addr.UnconfirmedBalance = 0
-	//		addr.UnconfirmedTxApperances = 0
-	//		addr.Transactions = append(addr.Transactions, transaction.Txid)
-	//
-	//		fmt.Println(addr.AddrStr)
-	//
-	//		database.IndexAdress(db, addr)
-	//	}
-	//	//transaction.Vout[i].ScriptPubKey.Addresses[0]
-	//}
-}
-
-//end result test 1 address
-func test() {
-
-	var addr insight.AddrIndex
-	fmt.Println(database.ViewAddress(db, "VdMPvn7vUTSzbYjiMDs1jku9wAh1Ri2Y1A"))
-	decoder := gob.NewDecoder(bytes.NewReader(database.ViewAddress(db, "VdMPvn7vUTSzbYjiMDs1jku9wAh1Ri2Y1A")))
-	decoder.Decode(&addr)
-
-	fmt.Printf("address: %s received: %f\n", addr.AddrStr, addr.TotalReceived)
-	//fmt.Println(addr)
-}
-
-// future opt might be using hex instead of TransactionVerbose
-// 2000 blocks takes the same size but 35 seconds instead of 39
-// for j := 0; j < len(block.Tx); j++ {
-// 	txhash, _ := chainhash.NewHashFromStr(block.Tx[j])
-// 	r := blockdata.GetRawTransaction(txhash)
-// 	txHex, _ := hex.DecodeString("010000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff3503d3fa520fe4b883e5bda9e7a59ee4bb99e9b1bc205b323031382d30392d31355431343a32343a32392e3237343736363538315a5dffffffff0294357700000000001976a914b722782e401ed7a31135580ada74962ef32e5cd288ac0000000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf90120000000000000000000000000000000000000000000000000000000000000000000000000")
+//func BuildDatabase() {
 //
-// 	database.AddRawTransaction(db, txHex, r)
-// }
+//	startblock := 5422072 - 1
+//	currentBlock := 5422072 + 10
+//	progressBar := pb.StartNew(currentBlock)
+//	wg := sync.WaitGroup{}
+//	wg.Add(currentBlock - startblock)
+//
+//	for i := int64(startblock); i < int64(currentBlock); i++ {
+//		go resolveBlockToDB(i, progressBar, &wg)
+//	}
+//
+//	wg.Wait()
+//	progressBar.Finish()
+//	test()
+//}
+//
+//func resolveBlockToDB(i int64, prBar *pb.ProgressBar, callerWG *sync.WaitGroup) {
+//	blockhash := blockdata.GetBlockHash(i)
+//	block, err := blockdata.GetBlock(blockhash)
+//	if err != nil {
+//		log.Fatal(err)
+//		return
+//	}
+//
+//	localWG := sync.WaitGroup{}
+//	localWG.Add(3)
+//
+//	// adding transactions to the "Transactions" bucket
+//	go func() {
+//		defer localWG.Done()
+//		for j := 0; j < len(block.Tx); j++ {
+//			txhash, _ := chainhash.NewHashFromStr(block.Tx[j])
+//			tx := blockdata.GetRawTransactionVerbose(txhash)
+//
+//			//Needs to be changed
+//			insighttx := insight.TxRawResult{
+//				tx,
+//				0.022,
+//				0.1,
+//				200.10,
+//			}
+//
+//			fmt.Println(insighttx)
+//
+//			database.AddTransaction(db, insighttx)
+//			resolveAddresses(tx)
+//		}
+//	}()
+//
+//	go func() {
+//		defer localWG.Done()
+//
+//		// bucket:"Blocks"  key:blockhash  value:blockVerboseResult
+//		database.AddBlock(db, blockhash.String(), block)
+//	}()
+//
+//	go func() {
+//		defer localWG.Done()
+//
+//		// bucket:"Blockheight" key:blockheight value:blockhash
+//		database.AddIndexBlockHeightWithBlockHash(db, block.Hash, block.Height)
+//	}()
+//
+//	localWG.Wait()
+//	callerWG.Done()
+//	prBar.Increment()
+//}
+//
+//// index adresses by calculating every transaction, balance etc
+//// example: https://explorer.viacoin.org/api/addr/Vrh9ro5WhykxrPPBe2cgyNiB2sAVqzkWjX
+//func resolveAddresses(transaction *btcjson.TxRawResult) {
+//
+//	var addr insight.AddrIndex
+//
+//	addr.AddrStr = transaction.Vout[0].ScriptPubKey.Addresses[0]
+//	addr.TotalReceived = transaction.Vout[0].Value
+//	addr.TotalReceivedSat = addr.TotalReceived * 100000000
+//	//address.TotalSent
+//	//address.TotalSentSat
+//	addr.UnconfirmedBalance = 0
+//	addr.UnconfirmedTxApperances = 0
+//	addr.Transactions = append(addr.Transactions, transaction.Txid)
+//
+//	//fmt.Println(addr.AddrStr)
+//	//fmt.Println(transaction.Txid)
+//	//fmt.Println(addr.TotalReceived)
+//	fmt.Println(transaction.Vin[0].ScriptSig)
+//
+//	// Check if address was already in the database
+//	// use old values to calculate the new values.
+//	// addrInDatabase has all the values of what is already in the database
+//	var addrInDatabase insight.AddrIndex
+//	addressInDatabase := database.ViewAddress(db, addr.AddrStr)
+//	if len(addressInDatabase) > 1 {
+//		decoder := gob.NewDecoder(bytes.NewReader(addressInDatabase))
+//		decoder.Decode(&addrInDatabase)
+//
+//		addr.TotalReceived += addrInDatabase.TotalReceived
+//		addr.TotalReceivedSat += addrInDatabase.TotalReceivedSat
+//		addr.Transactions = append(addr.Transactions, addrInDatabase.Transactions[0])
+//
+//		// delete old key in the database so the updated one can be inserted instead
+//		database.DeleteAddress(db, addr.AddrStr)
+//
+//	}
+//
+//	database.IndexAdress(db, addr)
+//
+//	//var addr address.Index
+//	//
+//	//for i := 0; i < len(transaction.Vout); i++ {
+//	//	for j := 0; i < len(transaction.Vout[i].ScriptPubKey.Addresses); j++ {
+//	//		addr.AddrStr = transaction.Vout[i].ScriptPubKey.Addresses[j]
+//	//		addr.TotalReceived = transaction.Vout[i].Value
+//	//		addr.TotalReceivedSat = addr.TotalReceived * 100000000
+//	//		//address.TotalSent
+//	//		//address.TotalSentSat
+//	//		addr.UnconfirmedBalance = 0
+//	//		addr.UnconfirmedTxApperances = 0
+//	//		addr.Transactions = append(addr.Transactions, transaction.Txid)
+//	//
+//	//		fmt.Println(addr.AddrStr)
+//	//
+//	//		database.IndexAdress(db, addr)
+//	//	}
+//	//	//transaction.Vout[i].ScriptPubKey.Addresses[0]
+//	//}
+//}
+//
+////end result test 1 address
+//func test() {
+//
+//	var addr insight.AddrIndex
+//	fmt.Println(database.ViewAddress(db, "VdMPvn7vUTSzbYjiMDs1jku9wAh1Ri2Y1A"))
+//	decoder := gob.NewDecoder(bytes.NewReader(database.ViewAddress(db, "VdMPvn7vUTSzbYjiMDs1jku9wAh1Ri2Y1A")))
+//	decoder.Decode(&addr)
+//
+//	fmt.Printf("address: %s received: %f\n", addr.AddrStr, addr.TotalReceived)
+//	//fmt.Println(addr)
+//}
--- cmd/rebuilddb/rebuild_test.go
+++ cmd/rebuilddb/rebuild_test.go
@@ -1,11 +1,34 @@
 package rebuilddb
 
-import "testing"
+import (
+	"testing"
+	"github.com/romanornr/cyberchain/mongodb"
+	"log"
+	"fmt"
+)
+
+func TestDropDatabase(t *testing.T) {
+	mongodb.DropDatabase()
+	session := mongodb.GetSession()
+
+	log.Println("Dropping old existing database")
+	mongodb.DropDatabase()
+
+	databases, _ := session.DatabaseNames()
+
+	for _, databases := range databases {
+		if databases == "Viacoin" {
+			fmt.Println("found")
+			t.Error("Old database still exists. Failed dropping.")
+		}
+	}
+	log.Println("Success dropped old database")
+}
 
 func TestBuildDatabase(t *testing.T) {
 	BuildDatabase()
 }
 
-func BenchmarkBuildDatabase(b *testing.B) {
-	BuildDatabase()
-}
+//func BenchmarkBuildDatabase(b *testing.B) {
+//	BuildDatabase()
+//}

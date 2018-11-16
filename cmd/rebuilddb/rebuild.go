071d34d435578e51bf4c3c788e7e328415b975b2
Romano <romanornr@gmail.com>
2018-11-17 00:14:36 +0100

    [rebuilddb] gix

--- cmd/rebuilddb/rebuild.go
+++ cmd/rebuilddb/rebuild.go
@@ -70,6 +70,10 @@ try to analyze this address: https://chainz.cryptoid.info/via/address.dws?369935
 	2000 blocks with transactions cost 3.275 seconds
 
 	2000 blocks with tx and a goroutine cost 2.56 seconds
+
+    // for account balance check http://127.0.0.1:8000/api/via/addr/Vn5maEzzZNPQ85rKFAgACRW98oiDtmMumG
+    // 6852caef331276d62c0de58ee430889c3926d9b5d832c7360dd9efe33fa1b6f6;11046;2014-07-20 00:04:49;10;2360.19357 <--block 11046
+	// f5a38ecb879748de37c4bd4ae3695ae6fe324a61c666eae3d547e736ae42ff62;11129;2014-07-20 00:40:49;10;2540.20557
 */
 
 func BuildDatabase() {
@@ -95,7 +99,7 @@ func BuildDatabase() {
 		newBlock.Reward = subsidy.CalcViacoinBlockSubsidy(int32(newBlock.Height), isMainChain)
 		newBlock.IsMainChain = isMainChain
 
-		mongodb.AddBlock(newBlock)
+		go mongodb.AddBlock(newBlock)
 
 		AddTransactions(txs, newBlock.Height)
 
